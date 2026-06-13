3044d714eef043dbe378206e9dbb676de8e14459
How <clicoc@users.noreply.github.com>
2026-06-13 18:35:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+# 定义 UUID 及 伪装路径
 base64 -d config > config.json
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
@@ -14,11 +14,9 @@ mv v ${RELEASE_RANDOMNESS}
 cat config.json | base64 > config
 rm -f config.json
 
-# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-TLS=${NEZHA_TLS:+'--tls'}
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} ${TLS}
-
-# 运行 nginx 和 v2ray
+# 运行 nginx 
 nginx
+
+# 运行 v2ray
 base64 -d config > config.json
 ./${RELEASE_RANDOMNESS} run
